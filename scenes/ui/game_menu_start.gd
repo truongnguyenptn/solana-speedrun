e8554f5b70fda5d0802c973e3df2c5bfd2e27451
truongnguyenptn <truongnguyen310.work@gmail.com>
2024-07-06 06:25:11 +0700

    feat: add wallet adapter

--- scenes/ui/game_menu_start.gd
+++ scenes/ui/game_menu_start.gd
@@ -1,10 +1,31 @@
 extends CanvasLayer
 
-
+@export var wallet_adapter: WalletAdapterUI
 func _ready():
+	
 	SolanaService.wallet.connect("on_logged_in",load_game)
+	SolanaService.wallet.connect("on_login_begin",pop_adapter)
+	
+	
+	wallet_adapter.visible = false
+	wallet_adapter.connect("on_provider_selected", handle_provider_selected)
+	wallet_adapter.connect("on_adapter_cancel",close_adapter)
+	
 	pass
 
+func pop_adapter() -> void:
+	wallet_adapter.visible = true
+	print("vkl")
+	print(SolanaService.wallet.wallet_adapter.get_available_wallets())
+	wallet_adapter.setup([0, 2])
+#
+func close_adapter() -> void:
+	wallet_adapter.visible = false
+
+func handle_provider_selected(id: int) -> void:
+	SolanaService.wallet.login_adapter(id)
+	pass
+	
 func _process(delta):
 	pass
 
